bb4772a5b9eb445c64a17224a3a82545ae44ff8c
Techno-Zee <work.me.30.4@gmail.com>
2026-01-31 20:10:03 +0700

    feat(shell_dashboard models,view & static) add dashboard block, menu & query

--- addons/shell_dashboard/controllers/controllers.py
+++ addons/shell_dashboard/controllers/controllers.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+from odoo.http import request
 
+class ZeeUi(http.Controller):
+    """Class to search and filter values in dashboard"""
 
-# class ShellDashboard(http.Controller):
-#     @http.route('/shell_dashboard/shell_dashboard', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+    @http.route('/api/shell_dashboard/dashboard_search', type='json', auth='user')
+    def dashboard_search(self, search_input):
+        """Filter dashboard blocks by name"""
+        blocks = request.env['dashboard.block'].sudo().search([
+            ('name', 'ilike', search_input)
+        ])
+        return blocks.ids
 
-#     @http.route('/shell_dashboard/shell_dashboard/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('shell_dashboard.listing', {
-#             'root': '/shell_dashboard/shell_dashboard',
-#             'objects': http.request.env['shell_dashboard.shell_dashboard'].search([]),
-#         })
-
-#     @http.route('/shell_dashboard/shell_dashboard/objects/<model("shell_dashboard.shell_dashboard"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('shell_dashboard.object', {
-#             'object': obj
-#         })
-
+    @http.route('/api/shell_dashboard/check_access', type='json', auth='user')
+    def check_access(self):
+        """Check access rights of the current user in custom dashboard groups"""
+        user = request.env.user
+        return {
+            "is_user": user.has_group("shell_dashboard.group_dashboard_user"),
+            "is_manager": user.has_group("shell_dashboard.group_dashboard_manager"),
+            "is_admin": user.has_group("shell_dashboard.group_dashboard_admin"),
+        }
